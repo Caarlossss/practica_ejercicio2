3cb046fc9bef6253e7a6edc6e9ba5a0f70deb689
Caarlossss <119401398+Caarlossss@users.noreply.github.com>
2022-12-13 08:36:13 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    if (3 > input.temperature()) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+})
